1bda6e18b0531861fb0e43f23be48e515b9d70a0
dburtonthompson <dburtonthompson@gmail.com>
2020-06-22 09:02:28 -0400

    added a read me and made file Subscriber.java

---
+++ README.md
@@ -0,0 +1,13 @@
+# Spring Boot - Accessing and adding data
+
+## Dependicies
+
+- Spring web
+- Spriing data JPA
+- H2 Database
+- Spring Boot Dev Tools
+- Thymeleaf
+
+### Dependencies
+
+Had to update my Java version in pom.xml to 1.11 to match my local JDK
---
+++ src/main/java/com/example/subscriberlist/subscriber/Subscriber.java
@@ -0,0 +1,64 @@
+package com.example.subscriberlist.subscriber;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+public class Subscriber {
+    @Id
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName, lastName, userName;
+    @Column
+
+    @CreationTimestamp
+    private Date signedUp;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Date getSignedUp() {
+        return signedUp;
+    }
+
+    public void setSignedUp(Date signedUp) {
+        this.signedUp = signedUp;
+    }
+
+    @Override
+    public String toString() {
+        return "Subscriber [firstName=" + firstName + ", id=" + id + ", lastName=" + lastName + ", signedUp=" + signedUp
+                + ", userName=" + userName + "]";
+    }
+
+}
